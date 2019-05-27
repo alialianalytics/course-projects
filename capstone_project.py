ee015dc573e0b15699d4b46d5859d58b5b530d75
Al-Bazergan <Ali.Al-Bazergan@informa.com>
2019-05-27 09:22:26 +0200

    adds a python script

---
+++ capstone_project.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone Project
+
+# ##### This notebook will be used for the Applied Science Capstone course.
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
+
